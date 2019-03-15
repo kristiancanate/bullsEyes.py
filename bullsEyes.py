5b4acbbb71f119d8d79dca86af7c6e24333d50e6
csPython <cspython@mbhscart6-23.mbhs.lab>
2019-03-15 09:41:43 -0400

    custom function to create circle

--- bullsEyes.py
+++ bullsEyes.py
@@ -1,12 +1,15 @@
 from graphics import *
 
+def circle(posX, posY, rad, color):
+    trgt = Circle(Point(posX,posY), rad)
+    trgt.setFill(color)
+    trgt.setOutline(color)
+    trgt.draw(bEWin)
+
 bEWin = GraphWin("bullsEyes", 300, 300)
 bEWin.setCoords(0, 0, 300, 300)
 
-trgt = Circle(Point(150,150), 100)
-trgt.setFill("red")
-trgt.setOutline("red")
-trgt.draw(bEWin)
+circle(150, 150, 100, "red")
 
 bEWin.getMouse()
 bEWin.close()
