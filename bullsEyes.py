2960d09f98bb69744d8c8314fbae62b985077ac5
csPython <cspython@mbhscart6-23.mbhs.lab>
2019-03-15 09:51:19 -0400

    created a bull's eye function

--- bullsEyes.py
+++ bullsEyes.py
@@ -6,10 +6,17 @@ def circle(posX, posY, rad, color):
     trgt.setOutline(color)
     trgt.draw(bEWin)
 
+def bEye(posX, posY, rad, col1, col2):
+    circle(posX, posY, rad, col1)
+    circle(posX, posY, rad-20, col2)
+    circle(posX, posY, rad-40, col1)
+    circle(posX, posY, rad-60, col2)
+    circle(posX, posY, rad-80, col1)
+
 bEWin = GraphWin("bullsEyes", 300, 300)
 bEWin.setCoords(0, 0, 300, 300)
 
-circle(150, 150, 100, "red")
+bEye(150, 150, 100, "red", "white")
 
 bEWin.getMouse()
 bEWin.close()
